3e5a9010700d1d3932eb51d5354c26e35171f1de
Zach Toben <zach.toben@gmail.com>
2018-06-28 09:14:49 -0500

    add media controls

--- src/index.js
+++ src/index.js
@@ -4,19 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const {ipcRenderer} = window.require('electron');
-
-ipcRenderer.on('play-pause', () => {
-  console.log('play/pause');
-});
-
-ipcRenderer.on('next', () => {
-  console.log('next');
-});
-
-ipcRenderer.on('previous', () => {
-  console.log('previous');
-});
-
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
